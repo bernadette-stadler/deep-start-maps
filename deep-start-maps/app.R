364058afff05e5a571be13265718ec2f11630f93
Bernadette Stadler <bstadler@g.harvard.edu>
2020-06-03 13:45:16 -0400

    Finalized first draft and published it. Need it insert code comments and make edits to the RMD version.

--- deep-start-maps/app.R
+++ deep-start-maps/app.R
@@ -29,32 +29,43 @@ location_data <- location_data %>% mutate(side = if_else(country == "Russia", "R
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("darkly"),
+                titlePanel(""),
 
     # Application title
-    titlePanel("Arms Control Negotiation Academy"),
-        sidebarPanel(width = 2,
-                     h5("This map shows major sites in the American and Russian nuclear weapons complexes, including military sites with deployed weapons as well as storage and production sites. Hover over a specific site for more information about it. Use the options below to select which sites are displayed on the map."),
-            checkboxGroupInput("strana",
-                               "Display",
+        column(2,
+               imageOutput("logo", width = "100px", height="100px"),
+               br(),
+               br(),
+               sidebarPanel(width=12, 
+               h5("This map shows major sites in the American and Russian nuclear weapons complexes, including military sites with deployed weapons as well as storage and production sites. Hover over a specific site for more information about it. Use the options below to select which sites are displayed on the map."),
+               br(), 
+               checkboxGroupInput("strana",
+                               "Country",
                                choices = c("Russia", "United States"),
                                selected = c("Russia", "United States")), 
                                br(),
             checkboxGroupInput("type", 
-                               "Display", 
+                                "Weapon type", 
                                choices = c("NSNW", "Strategic"), 
                                selected = c("NSNW", "Strategic")
-            )
+            ))
         ),
         
-        # add a sidebar panel with multiple checkboxes that the viewer can
-        # click to see multiple variables showed
-        
-        mainPanel(
-            plotlyOutput("plot1")
-            
-            # display the plot (created in the server)
-        )
+        column(10,
+               br(), 
+            plotlyOutput("plot1", height="800px"),
+        ), 
+    br(), 
+    column(12, 
+           offset = 3, 
+           h5("Sources:"),  
+           h5("Hans M. Kristensen & Robert S. Norris (2017) Worldwide deployments of nuclear weapons, 
+            2017, Bulletin of the Atomic Scientists, 73:5, 289-297,"), 
+           h5("Hans M. Kristensen & Matt Korda (2020) Russian nuclear forces, 2020, 
+            Bulletin of the Atomic Scientists, 76:2, 102-117,"),
+           h5("The CSIS Missile Threat Website"))
 )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -93,18 +104,38 @@ server <- function(input, output) {
             add_markers(
             text = ~paste('</br> Name: ', base_location,
                           '</br> Delivery System: ', delivery_system,
-                          '</br> Weapon: ', warhead), 
+                          '</br> Warhead: ', warhead, 
+                          '</br> Warhead type: ', type), 
              marker = list(color = 'rgb(0,0,0)', line = list(color = 'rgb(0,0,0)')), symbol = ~status, size = I(14), hoverinfo = "text"
         ) %>% 
             layout(
+                title = list(text ="Deep START exercise: U.S. and Russian Nuclear Weapons Deployment", 
+                             y =0.9, x = 0.55),
+                titlefont = list(
+                    family = "Segoe UI",
+                    size = 18,
+                    color = 'rgb(255,255,255'), 
                 geo = g, 
-                legend = list(x = 0.8, 
+                legend = list(x = 0.8,
                               y = 0.2, 
                               bgcolor = 'rgb(255, 255, 255)', 
                               bordercolor = 'rgb(0,0,0)', 
                               borderwidth = 2), 
                 paper_bgcolor='rgb(34,34,34)')
     })
+    
+    output$logo <- renderImage({
+        
+        # Use renderImage, assign it to output$image,
+        # which corresponds with the imageOutput("Image") above.
+        
+        list(
+            src = "ACONA.png",
+            contentType = "image/png", 
+            width = 96, 
+            height = 72
+        )
+    }, deleteFile = FALSE)
 }
 
 # Run the application 
